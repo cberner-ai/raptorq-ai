bebbe9435ae5067a161bbc77a72b1eef697f5e5a
Hitch <hitch@localhost>
2026-06-18 05:32:43 +0000

    Lower Packed GF2 SIMD Gate

--- src/gf2.rs
+++ src/gf2.rs
@@ -13,7 +13,7 @@ use core::arch::x86_64::{
 
 use crate::matrix::BinaryMatrix;
 
-const WIDE_BINARY_ROW_POPCOUNT_MIN_WORDS: usize = 256;
+const WIDE_BINARY_ROW_POPCOUNT_MIN_WORDS: usize = 128;
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct PackedBinaryRows {
     height: usize,
